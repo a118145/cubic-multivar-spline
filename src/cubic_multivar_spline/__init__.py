dab45a5ec65aca5d9c718ca72344f875452c522b
a118145 <arne@hansen-doerr.de>
2026-02-19 18:27:51 +0100

    chore: initial packaging scaffolding

---
+++ src/cubic_multivar_spline/__init__.py
@@ -0,0 +1,21 @@
+"""cubic_multivar_spline
+
+A tiny placeholder package for cubic multivariate spline utilities.
+"""
+
+__version__ = "0.1.0"
+
+__all__ = ["version", "example_function"]
+
+version = __version__
+
+def example_function(x):
+    """Example placeholder function.
+
+    Parameters
+    - x: numeric
+
+    Returns
+    - x squared
+    """
+    return x * x
---
+++ tests/test_basic.py
@@ -0,0 +1,9 @@
+from cubic_multivar_spline import version, example_function
+
+
+def test_version_is_string():
+    assert isinstance(version, str)
+
+
+def test_example_function():
+    assert example_function(4) == 16
